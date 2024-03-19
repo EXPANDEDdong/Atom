ed79c58c7dec633cfd4ff726d7bf2ac757b2a8cf
DongsExpanding <william.sjodin05@gmail.com>
2024-03-19 13:35:08 +0100

    Change root page for new changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@
 import PostFeed from "@/components/PostFeed";
 import PostFeedSkeleton from "@/components/PostFeedSkeleton";
 import PostForm from "@/components/PostForm";
-import { getCurrentUser, getPosts } from "@/utils/actions";
+import { FetchParameters, getCurrentUser, getPosts } from "@/utils/actions";
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
+import { devNull } from "os";
 import { Suspense } from "react";
 
 export default async function Home() {
@@ -18,23 +19,26 @@ export default async function Home() {
 
   const queryKey = "posts";
 
+  const fetchParams: FetchParameters<"all"> = {
+    type: "all",
+  };
+
   await queryClient.prefetchInfiniteQuery({
     queryKey: [queryKey],
     queryFn: async ({ pageParam }) => {
-      return await getPosts(currentUser, pageParam);
+      return await getPosts(fetchParams, currentUser, pageParam);
     },
     initialPageParam: 0,
   });
-
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="h-full lg:w-2/3">
-        <PostForm />
+        <PostForm queryKey={queryKey} replyToId={null} />
         <HydrationBoundary state={dehydrate(queryClient)}>
           <Suspense fallback={<PostFeedSkeleton />}>
             <PostFeed
               currentUser={currentUser}
-              fetchFunction={getPosts.bind(null, currentUser)}
+              fetchParameters={fetchParams}
               queryKey={queryKey}
               isReplies={false}
             />
