5967527e824eb1ad679451c82ee1a32c0ea07c62
DongsExpanding <william.sjodin05@gmail.com>
2024-04-14 12:19:10 +0200

    Expanded user page
    Added metadata generation, changing the tab name to the current users page.
    
    You can no longer see the page of someone who has blocked you.
    
    Also added proper buttons for:
    - Following and unfollowing
    - Blocking and unblocking
    - Starting a new chat
    - Editing user info if its your own profile

--- app/user/[username]/layout.tsx
+++ app/user/[username]/layout.tsx
@@ -2,7 +2,6 @@
 
 import { getUserPageProfile } from "@/utils/actions";
 import { notFound } from "next/navigation";
-
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Label } from "@/components/ui/label";
@@ -10,24 +9,68 @@ import { Suspense } from "react";
 import PostFeedSkeleton from "@/components/PostFeedSkeleton";
 import NewDmButton from "@/components/NewDmButton";
 import BlockButton from "@/components/BlockButton";
-import { followUser } from "./actions";
-import { Button } from "@/components/ui/button";
+import FollowButton from "@/components/FollowButton";
+import EditButton from "@/components/EditButton";
+import BackButton from "@/components/BackButton";
+import type { Metadata, ResolvingMetadata } from "next";
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
 
-export default async function Layout({
-  children,
-  params,
-}: {
+type Props = {
   children: React.ReactNode;
   params: { username: string };
-}) {
+};
+
+export async function generateMetadata(
+  { children, params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const username = params.username;
+
+  const cookieStore = cookies();
+  const db = createClient(cookieStore);
+
+  const { data } = await db
+    .from("profiles")
+    .select("displayname")
+    .eq("username", username)
+    .maybeSingle();
+
+  if (!data || !data.displayname)
+    return {
+      title: "Atom | Unknown user",
+    };
+
+  const name = data.displayname;
+
+  const metadata: Metadata = {
+    title: `Atom | ${name}`,
+  };
+
+  return metadata;
+}
+
+export default async function Layout({ children, params }: Props) {
   const userPage = await getUserPageProfile(params.username);
 
   if (!userPage) {
     notFound();
   }
+
+  if (userPage === "blocked")
+    return (
+      <main className="w-full flex flex-col items-center justify-center py-14">
+        <p className="text-2xl text-center">
+          {params.username} has blocked you.
+        </p>
+
+        <BackButton />
+      </main>
+    );
+
   return (
-    <main className="pw-full flex flex-col items-center">
-      <div className="w-1/2">
+    <main className="w-full flex flex-col items-center">
+      <div className="lg:w-1/2 sm:w-2/3 w-full">
         <div className="flex flex-col gap-4">
           <div className="w-full flex justify-between space-x-4 pt-8">
             <div className="relative flex h-32 w-32 overflow-hidden rounded-full -z-10">
@@ -48,17 +91,27 @@ export default async function Layout({
             </div>
           </div>
           <div className="flex flex-col gap-1">
+            <EditButton userId={userPage.id} />
             <Label htmlFor="bio">Description</Label>
             <div id="bio" className="px-2 py-1">
               <p className="text-lg">{userPage.description}</p>
             </div>
           </div>
-          <form action={followUser.bind(null, userPage.id)}>
-            <Button type="submit">Follow</Button>
-          </form>
-          <div className="w-full flex flex-row gap-2">
-            <NewDmButton userId={userPage.id} />
-            <BlockButton username={userPage.displayname} userId={userPage.id} />
+
+          <div className="w-full flex flex-col gap-2">
+            <FollowButton
+              hasFollowed={userPage.has_followed}
+              username={userPage.displayname}
+              userId={userPage.id}
+            />
+            <div className="w-full flex flex-row gap-2">
+              <NewDmButton userId={userPage.id} />
+              <BlockButton
+                hasBlocked={userPage.has_blocked}
+                username={userPage.displayname}
+                userId={userPage.id}
+              />
+            </div>
           </div>
         </div>
         <Separator className="mt-4" />
