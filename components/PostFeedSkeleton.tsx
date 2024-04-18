8d08a17b2ff5d5f14105bc7ece968c9e720e348d
DongsExpanding <william.sjodin05@gmail.com>
2024-04-18 10:13:48 +0200

    Post feed skeleton small styling change

--- components/PostFeedSkeleton.tsx
+++ components/PostFeedSkeleton.tsx
@@ -4,16 +4,16 @@ import { Skeleton } from "./ui/skeleton";
 export default function PostFeedSkeleton() {
   const arr = [1, 2, 3, 4, 5];
   return (
-    <main className="w-full h-full flex flex-col gap-2">
+    <main className="flex flex-col items-center w-full h-full gap-2">
       {arr.map((d, i) => (
-        <Card key={i} className="w-full">
+        <Card key={i} className="w-full lg:w-1/2 sm:w-2/3">
           <CardHeader>
             <Skeleton className="h-4 w-14" />
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
             <Skeleton className="w-full h-4" />
             <Skeleton className="w-11/12 h-4" />
-            <Skeleton className="w-full rounded-lg h-48 mt-2" />
+            <Skeleton className="w-full h-48 mt-2 rounded-lg" />
           </CardContent>
         </Card>
       ))}
