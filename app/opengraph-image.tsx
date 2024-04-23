b564b7c83e59318199b8e4c64dff1d96ba76fc98
EXPANDEDdong <william.sjodin05@gmail.com>
2024-04-23 20:39:09 +0200

    Opengraph image generation test

---
+++ app/opengraph-image.tsx
@@ -0,0 +1,53 @@
+import { ImageResponse } from "next/og";
+
+export const alt = "Atom image.";
+export const size = {
+  width: 800,
+  height: 400,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#000",
+          backgroundImage: "linear-gradient(to bottom, #000, #1c1c1c)",
+          fontSize: 32,
+          fontWeight: 600,
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="96"
+          height="96"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#fff"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="1" />
+          <path d="M20.2 20.2c2.04-2.03.02-7.36-4.5-11.9-4.54-4.52-9.87-6.54-11.9-4.5-2.04 2.03-.02 7.36 4.5 11.9 4.54 4.52 9.87 6.54 11.9 4.5Z" />
+          <path d="M15.7 15.7c4.52-4.54 6.54-9.87 4.5-11.9-2.03-2.04-7.36-.02-11.9 4.5-4.52 4.54-6.54 9.87-4.5 11.9 2.03 2.04 7.36.02 11.9-4.5Z" />
+        </svg>
+        <div style={{ marginTop: 40, color: "#fff" }}>Atom</div>
+        <div style={{ marginTop: 10, color: "#d4d4d4", fontSize: 24 }}>
+          Connecting you and like nobody else
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
